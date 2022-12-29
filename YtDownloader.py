0355c3c34ce8060909d198980bacee90292fa32c
Tihomir <t.ivanov2003@abv.bg>
2022-12-29 18:46:19 +0200

    Implemented basic GUI

--- YtDownloader.py
+++ YtDownloader.py
@@ -13,43 +13,50 @@ def OnProgress(stream:Stream, chunk: bytes, bytes_remaining: int):
 def OnDownloadComplete(arg1,arg2):
     print("Video downloaded!")
 
-def DownloadVideo(video_link:str):
-    yt_instance = YouTube(video_link)
-    yt_instance.register_on_progress_callback(OnProgress)
-    yt_instance.register_on_complete_callback(OnDownloadComplete)
+def DownloadVideo(video_link:str,resolution:str):
+    try:
+        yt_instance = YouTube(video_link)
+        yt_instance.register_on_progress_callback(OnProgress)
+        yt_instance.register_on_complete_callback(OnDownloadComplete)
 
-    ytStream = yt_instance.streams.filter(file_extension="mp4").first()
-    ytStream.download(output_path="./yt_downloads")
+        ytStream = yt_instance.streams.filter(file_extension="mp4",resolution=resolution).first()
+        ytStream.download(output_path="./yt_downloads")
+
+    except: print("Error downloading video")
 
 def Init():
+    global root 
     root = Tk()
     root.resizable(width=False,height=False)
     root.title("YouTube Downloader")
-    root.geometry("500x500")
-    root.config(bg="black")
+    root.geometry("640x480")
 
-    frm = ttk.Frame(root)
-    frm.grid()
+    ttk.Label(root, text="Enter a video link").grid(column=1, row=2,padx=20)
+    global video_link_text
+    video_link_text = Text(root, height=1, width=35)
+    video_link_text.grid(column=1,row=3)
+    video_link_text.bind("<KeyRelease>",SelectVideo)
 
-    ttk.Label(frm, text="Enter a video link").grid(column=1, row=2,padx=20)
-    video_link_text = Text(frm, height=1, width=35)
-    video_link_text.grid(column=2,row=2)
-
-    combobox = ttk.Combobox(frm,textvariable="360p")
-    combobox['values'] = ('180p', '240p', '360p', '480p', '720p', '1080p')
-    combobox['state'] = 'readonly'
-    combobox.bind('<<ComboboxSelected>>', SelectResolution(combobox.get()))
-
-    #btnDownload = ttk.Button(frm, text="Download", command=DownloadVideo(video_link_text.get("1.0",END)))
-    #btnDownload.grid(column=0, row=1)
+    root.grid_rowconfigure(10, weight=1)
+    ttk.Button(root, text="Quit", command=root.destroy).grid(row=10,column=3,sticky="es")
     #add button when stopped writing and input is a link
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=3)
-
     root.mainloop()
+
+selectedResolution = ""
+videoLink = ""
+
+def SelectVideo(selectedVideoLink:str):
+    videoLink = selectedVideoLink
+    combobox = ttk.Combobox(root,textvariable="360p")
+    combobox['values'] = ('144p', '240p', '360p', '480p', '720p', '1080p')
+    combobox['state'] = 'readonly'
+    combobox.bind('<<Selected>>', SelectResolution)
+    combobox.grid(column=2,row=3)
 
 def SelectResolution(resolution:str):
     selectedResolution = resolution
-    
+    btnDownload = ttk.Button(root, text="Download", command=DownloadVideo(videoLink,selectedResolution))
+    btnDownload.grid(row=10, column=0,sticky="ws")
 
 Init()
 #ytVideoLink = "https://www.youtube.com/watch?v=0qacFnuXSF8"
